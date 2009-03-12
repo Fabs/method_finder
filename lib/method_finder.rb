535c32775a021c3d47787100a3199034eb327893
Rafael Schouery <schouery@gmail.com>
2009-03-12 10:20:37 -0300

    Refactoring

--- lib/method_finder.rb
+++ lib/method_finder.rb
@@ -1,29 +1,39 @@
 module Kernel
-  # Suppresses warnings within a given block.
-  def with_warnings_suppressed
-    saved_verbosity = $-v
-    $-v = nil
-    yield
-  ensure
-    $-v = saved_verbosity
-  end
 
   def match_method(object,params,expected)
     with_warnings_suppressed do
       object.methods.select do |method|
-        begin
-          clone = object
-          begin
-            clone = object.clone
-          rescue
-          end
-          exp = "clone.#{method}(#{params.join(',')})"
-          result = eval(exp)
-          result == expected
-        rescue
-        end        
+        test_method(method, object, params, expected)
       end.sort
     end
   end
-
+  
+  private
+    # Suppresses warnings within a given block.
+    def with_warnings_suppressed
+      saved_verbosity = $-v
+      $-v = nil
+      yield
+    ensure
+      $-v = saved_verbosity
+    end
+  
+    def try_clone(object)
+      begin
+        object.clone
+      rescue
+        object
+      end
+    end
+  
+    def test_method(method, object, params, expected)
+      begin
+        clone = try_clone object
+        exp = "clone.#{method}(#{params.join(',')})"
+        result = eval(exp)
+        result == expected    
+      rescue
+      end
+    end
+    
 end
